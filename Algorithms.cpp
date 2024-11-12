92f77cd230d56da9d548d8ae47813a33a5890102
Saprrow <sparrow851.h@gmail.com>
2024-11-13 00:08:37 +0200

    first commit

---
+++ Algorithms.cpp
@@ -0,0 +1,89 @@
+﻿#pragma comment(lib, "bcrypt.lib")
+
+#include <windows.h>
+#include <stdio.h>
+#include <bcrypt.h>
+#include <ntstatus.h>
+#include <conio.h>
+
+int main()
+{
+	ULONG algCount;
+	BCRYPT_ALGORITHM_IDENTIFIER* pAlgList = NULL;
+
+	if (BCryptEnumAlgorithms(BCRYPT_HASH_OPERATION, &algCount, &pAlgList, 0) == STATUS_SUCCESS) {
+		wprintf(L"Hashing algorithms\n");
+		wprintf(L"~~~~~~~~~~~~~~~~~~\n");
+
+		for (int i = 0; i < algCount; i++)
+			wprintf(L"%s\n", pAlgList[i].pszName);
+
+		BCryptFreeBuffer(pAlgList);
+	}
+	else
+		wprintf(L"Could not retrieve CNG hashing algorithms.");
+
+	if (BCryptEnumAlgorithms(BCRYPT_CIPHER_OPERATION, &algCount, &pAlgList, 0) == STATUS_SUCCESS) {
+		wprintf(L"\nCipher algorithms\n");
+		wprintf(L"~~~~~~~~~~~~~~~~~~\n");
+
+		for (int i = 0; i < algCount; i++)
+			wprintf(L"%s\n", pAlgList[i].pszName);
+
+		BCryptFreeBuffer(pAlgList);
+	}
+	else
+		wprintf(L"Could not retrieve CNG cipher algorithms.");
+
+	if (BCryptEnumAlgorithms(BCRYPT_ASYMMETRIC_ENCRYPTION_OPERATION, &algCount, &pAlgList, 0) == STATUS_SUCCESS) {
+		wprintf(L"\nAsymetric encription algorithms\n");
+		wprintf(L"~~~~~~~~~~~~~~~~~~\n");
+
+		for (int i = 0; i < algCount; i++)
+			wprintf(L"%s\n", pAlgList[i].pszName);
+
+		BCryptFreeBuffer(pAlgList);
+	}
+	else
+		wprintf(L"Could not retrieve CNG asymetric encription algorithms.");
+
+	if (BCryptEnumAlgorithms(BCRYPT_SECRET_AGREEMENT_OPERATION, &algCount, &pAlgList, 0) == STATUS_SUCCESS) {
+		wprintf(L"\nSecret agreement algorithms\n");
+		wprintf(L"~~~~~~~~~~~~~~~~~~\n");
+
+		for (int i = 0; i < algCount; i++)
+			wprintf(L"%s\n", pAlgList[i].pszName);
+
+		BCryptFreeBuffer(pAlgList);
+	}
+	else
+		wprintf(L"Could not retrieve CNG secret agreement algorithms.");
+
+	if (BCryptEnumAlgorithms(BCRYPT_SIGNATURE_OPERATION, &algCount, &pAlgList, 0) == STATUS_SUCCESS) {
+		wprintf(L"\nSignature algorithms\n");
+		wprintf(L"~~~~~~~~~~~~~~~~~~\n");
+
+		for (int i = 0; i < algCount; i++)
+			wprintf(L"%s\n", pAlgList[i].pszName);
+
+		BCryptFreeBuffer(pAlgList);
+	}
+	else
+		wprintf(L"Could not retrieve CNG signature algorithms.");
+
+	if (BCryptEnumAlgorithms(BCRYPT_RNG_OPERATION, &algCount, &pAlgList, 0) == STATUS_SUCCESS) {
+		wprintf(L"\nRandom number generator\n");
+		wprintf(L"~~~~~~~~~~~~~~~~~~\n");
+
+		for (int i = 0; i < algCount; i++)
+			wprintf(L"%s\n", pAlgList[i].pszName);
+
+		BCryptFreeBuffer(pAlgList);
+	}
+	else
+		wprintf(L"Could not retrieve CNG random number generator algorithms.");
+
+	_getch();  // çàäåðæêà ýêðàíà
+
+	return 0;
+}
---
+++ Providers.cpp
@@ -0,0 +1,51 @@
+﻿#include <windows.h>
+#include <bcrypt.h>
+#include <stdio.h>
+
+#pragma comment(lib, "bcrypt.lib")
+
+#ifndef NT_SUCCESS
+#define NT_SUCCESS(Status) ((NTSTATUS)(Status) >= 0)
+#endif
+
+void EnumProviders1()
+{
+	NTSTATUS status;
+	ULONG cbBuffer = 0;
+	PCRYPT_PROVIDERS pBuffer = NULL;
+
+	/*
+	Get the providers, letting the BCryptEnumRegisteredProviders
+	function allocate the memory.
+	*/
+	status = BCryptEnumRegisteredProviders(&cbBuffer, &pBuffer);
+
+	if (NT_SUCCESS(status)) 
+	{
+		if (pBuffer != NULL) 
+		{
+			// Enumerate the providers.
+			for (ULONG i = 0; i < pBuffer->cProviders; i++) 
+			{
+				wprintf(L"%s\n", pBuffer->rgpszProviders[i]);
+			}
+		}
+	}
+
+	else 
+	{
+		printf("BCryptEnumRegisteredProviders failed with error code 0x%08x\n", status);
+	}
+
+	// Free the memory
+	if (NULL != pBuffer)
+	{
+		BCryptFreeBuffer(pBuffer);
+	}
+}
+
+int main() 
+{
+	EnumProviders1();
+	return 0;
+}
